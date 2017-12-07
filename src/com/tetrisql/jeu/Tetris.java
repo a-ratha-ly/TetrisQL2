38839ae7354738108e2ccac1e3b9b7f0a4b68638
Audrey LY <a.ratha.ly@gmail.com>
2017-12-07 21:51:51 +0100

    déplacer gauche droite

--- src/com/tetrisql/jeu/Tetris.java
+++ src/com/tetrisql/jeu/Tetris.java
@@ -1,14 +1,43 @@
 package com.tetrisql.jeu;
+import java.lang.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Tetris extends java.applet.Applet implements Runnable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	public Jeu jeu;
+	private volatile Thread thread; 
+	public boolean fin;
+	public boolean stop;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//démarrer le jeu
+	public void start() {
+		if(thread == null) {
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	//arrêter le jeu
+	public void stop() {
+		if(thread != null && thread.isAlive()) {
+			thread.interrupt();
+		}
+		thread = null;
+	}
+	
+	
+
+
+
 }
